3a84c18e82bc62d35b97cc0e9aff752ced84dd87
Martin Ribelotta <martinribelotta@gmail.com>
2014-01-25 18:40:59 -0300

    Change simple pointer to use pointer to pointer in example

--- app/main.c
+++ app/main.c
@@ -1,13 +1,15 @@
 #include "sysent.h"
 
-const char *build_date = __DATE__;
-const char *build_time = __TIME__;
+const char *datetime[] = {
+  __DATE__,
+  __TIME__
+};
 
 volatile int i;
 
 void banner(void) {
   for (i = 0; i < 10; i++)
-    syscalls.printf("Hello world on %s at %s (%d)\n", build_date, build_time, i);
+    syscalls.printf("Hello world on %s at %s (%d)\n", datetime[0], datetime[1], i);
 }
 
 void (*const trampoline)(void) = banner;
